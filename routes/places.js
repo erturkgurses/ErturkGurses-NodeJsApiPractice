f63e0b9f375d9b1886da8d9a8dc698234efd0b44
Ertürk Gürses <erturkgurses@hotmail.com>
2020-08-21 14:23:39 +0300

    Added get/post/delete methods to routes

--- routes/places.js
+++ routes/places.js
@@ -1,20 +1,227 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const router = express.Router();
 
+const VisitedPlace = require("../models/visitedPlace");
+const ToVisitPlace = require("../models/toVisitPlace");
+const toVisitPlace = require("../models/toVisitPlace");
+
+//visited Methods
+
+//Gel all visited places
 router.get("/visited", function (req, res) {
-    res.send("Visited sayfasına get isteği atıldı");
+    VisitedPlace.find()
+        .then((places) => {
+            res.json(places);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
 });
 
-router.get("/toVisit", function (req, res) {
-    res.send("ToVisit sayfasına get atıldı");
+//Get visited place by id
+router.get("/visited/:placeID", function (req, res) {
+    const id = req.params.placeID
+    VisitedPlace.findById({
+            _id: id
+        })
+        .then((places) => {
+            res.json(places);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
 });
 
+//Create a new visited place
 router.post("/visited", function (req, res) {
-    res.send("Visited sayfasına post isteği atıldı");
+    const {
+        name,
+        isOutdoor,
+        adress,
+        lastVisitDate,
+        willingnessScoreToVisitAgain,
+        numberOfVisit,
+        lastScore,
+        note,
+    } = req.body;
+
+    const newVisitedPlace = new VisitedPlace({
+        _id: new mongoose.Types.ObjectId(),
+        name: name,
+        isOutdoor: isOutdoor,
+        adress: adress,
+        lastVisitDate: lastVisitDate,
+        willingnessScoreToVisitAgain: willingnessScoreToVisitAgain,
+        numberOfVisit: numberOfVisit,
+        lastScore: lastScore,
+        note: note,
+    });
+
+    newVisitedPlace
+        .save()
+        .then((data) => {
+            res.send(data);
+        })
+        .catch((err) => {
+            res.status(500).send(err);
+        });
 });
 
+//Delete a visited place by id
+router.delete('/visited/:placeID', (req, res, next) => {
+    const id = req.params.placeID;
+    console.log(id)
+    VisitedPlace.deleteOne({
+            _id: id
+        }).exec()
+        .then(result => {
+            console.log(result)
+            res.status(200).json({
+                message: "Visited place is deleted",
+                request: {
+                    type: 'Delete by id'
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+// Delete all visited places
+router.delete("/visited", function (req, res, next) {
+    VisitedPlace.deleteMany({
+
+    }).exec().then(result => {
+        console.log(result);
+        res.status(200).json({
+            message: "All visited places are deleted",
+            request: {
+                type: 'Delete all'
+            }
+        });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+})
+
+
+
+
+
+
+
+
+
+
+//toVisit Methods
+
+//Get all toVisit places
+router.get("/toVisit", function (req, res) {
+    toVisitPlace.find()
+        .then((places) => {
+            res.json(places);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+//Get toVisit place by id
+router.get("/toVisit/:placeID", function (req, res) {
+    const id = req.params.placeID
+    toVisitPlace.findById({
+            _id: id
+        })
+        .then((places) => {
+            res.json(places);
+        })
+        .catch((err) => {
+            res.json(err);
+        });
+});
+
+// Create a new toVisit place
 router.post("/toVisit", function (req, res) {
-    res.send("ToVisit sayfasına post atıldı");
+    const {
+        name,
+        isOutdoor,
+        isVisited,
+        adress,
+        lastVisitDate,
+        willingnessScore,
+        expectationsScore,
+    } = req.body;
+
+    const newToVisitPlace = new ToVisitPlace({
+        name: name,
+        isOutdoor: isOutdoor,
+        isVisited: isVisited,
+        adress: adress,
+        lastVisitDate: lastVisitDate,
+        willingnessScore: willingnessScore,
+        expectationsScore: expectationsScore,
+    });
+
+    newToVisitPlace
+        .save()
+        .then((data) => {
+            res.send(data);
+        })
+        .catch((err) => {
+            res.status(500).send(err);
+        });
 });
 
+// Delete a toVisit place by id
+router.delete("/toVisit/:placeID", function (req, res, next) {
+    const id = req.params.placeID;
+    console.log(id)
+    ToVisitPlace.deleteOne({
+            _id: id
+        }).exec()
+        .then(result => {
+            console.log(result)
+            res.status(200).json({
+                message: "To visit place is deleted",
+                request: {
+                    type: 'Delete by id'
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+//Delete all toVisit places
+router.delete("/toVisit", function (req, res, next) {
+    ToVisitPlace.deleteMany({
+
+    }).exec().then(result => {
+        console.log(result);
+        res.status(200).json({
+            message: "All ToVisit places are deleted",
+            request: {
+                type: 'Delete all'
+            }
+        });
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+})
 module.exports = router;
