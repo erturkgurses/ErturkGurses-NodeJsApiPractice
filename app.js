e7a23f22f2a0aab1ce83f31cf7ed628d9ce66514
Ertürk Gürses <erturkgurses@hotmail.com>
2020-08-21 14:23:59 +0300

    Imported body-parser

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const bodyParser = require("body-parser");
+ 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const placesRoutes = require("./routes/places");
 
